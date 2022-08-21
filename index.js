304775659837af4c045cc818f520340e7332454e
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-08-21 20:35:43 +0100

    Second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+export default class Grid {
+    constructor(width = 0, height = 0) {
+        this.width = width;
+        this.height = height;
+    }
+};
---
+++ testGrid.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import Grid from './index.js';
+import { strict as assert } from 'node:assert';
+
+let g = new Grid(3, 3);
+
+console.log(g);
+assert(g);
