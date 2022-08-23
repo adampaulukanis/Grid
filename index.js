83aae25b921c3e376045f8462c5c263a38745b25
Adam Paulukanis <adam.paulukanis@gmail.com>
2022-08-23 20:55:34 +0100

    Some improvements
    + class Grid
    + my own (simple and fast) test framework
    + class Vector and tests for it all

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 export default class Grid {
     constructor(width = 0, height = 0) {
+        this.space = new Array(width * height);
         this.width = width;
         this.height = height;
+    }
+
+    isInside(vector) {
+        return vector.x >= 0 && vector.x < this.width &&
+               vector.y >= 0 && vector.y < this.height;
+    }
+
+    get(vector) {
+        return this.space[vector.x + this.width * vector.y]
+    }
+
+    set(vector, value) {
+        this.space[vector.x + this.width * vector.y] = value;
+        return true;
     }
 };
--- testGrid.js
+++ testGrid.js
@@ -3,7 +3,60 @@
 import Grid from './index.js';
 import { strict as assert } from 'node:assert';
 
-let g = new Grid(3, 3);
+let db = [];
+function it(text, fun) {
+    db.push({ text, fun });
+}
+function runTests() {
+    function response(f) { if (f) return 'OK'; else return 'NOK'; };
+    db.forEach(test => {
+        console.log(`${response(test.fun())} = ${test.text}`);
+    });
+}
 
-console.log(g);
-assert(g);
+
+class Vector {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(other) {
+        return new Vector(this.x + other.x, this.y + other.y);
+    }
+};
+
+let v = new Vector(1, 1);
+it('test Vector', function () {
+    let test = (v.x && v.x === 1 && v.y && v.y === 1);
+    assert(test);
+    return test;
+});
+let v2 = new Vector(-1, -2);
+let v3 = new Vector(0, -1);
+it('test if Vectors can add', function () {
+    let test = ((v.plus(v2)).x === v3.x && (v.plus(v2)).y === v3.y);
+    assert(test);
+    return test;
+}); 
+
+let grid = new Grid(5, 5);
+it('grid: if not set returns undefined', function () {
+    assert(grid.get(new Vector(1, 1)) === undefined);
+    return (grid.get(new Vector(1, 1)) === undefined);
+});
+
+it('grid: test if set works', function () {
+    grid.set(new Vector(1, 1), "X");
+    assert(grid.get(new Vector(1, 1)) === "X");
+    return (grid.get(new Vector(1, 1)) === "X");
+});
+
+
+it('grid: isInside()', function () {
+    let test = (grid.isInside(v));
+    assert(test);
+    return test;
+});
+
+runTests();
